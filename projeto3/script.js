56b6ab42387a22339213257db6a78c862e4cb895
Luan <luangf222@gmail.com>
2023-06-15 03:30:29 -0300

    att

--- projeto3/script.js
+++ projeto3/script.js
@@ -1,66 +1,16 @@
-function verificar(){
-    var data=new Date()
-    var ano=data.getFullYear()
-    var fano=document.getElementById('ano')
-    var res=document.querySelector('div#res')
-    if(fano.value.length == 0 || fano.value > ano){
-        alert('[ERRO] Verifique os dados e tente novamente')
+function contar(){
+    var inicio=document.querySelector('input#inicio')
+    var fim=document.querySelector('input#fim')
+    var passo=document.querySelector('input#passo')
+    var all=`${inicio.value} ${fim.value} ${passo.value}`
+    // alert(all)
+    if(inicio.value == ''){
+        res.innerHTML='<p>Impossível contar</p>'
+    }else if(fim.value == ''){
+        res.innerHTML='<p>Impossível contar</p>'
+    }else if(passo.value == ''){
+    res.innerHTML='<p>Impossível contar</p>'
     }else{
-        var fsex=document.getElementsByName('sexo')
-        // Number(fano.value) bm
-        var idade=ano-fano.value
-        var genero=''
-        var img=document.createElement('img')
-        img.setAttribute('id','foto')
-        if(fsex[0].checked){
-            genero='h'
-            if(idade >= 0 && idade < 10){
-                img.setAttribute('src', 'img/bebe-m.png')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }else if(idade < 21){
-                img.setAttribute('src', 'img/jovem-m.png')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }else if(idade < 75){
-                img.setAttribute('src', 'img/adulto-m.png')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }else{
-                img.setAttribute('src', 'img/idoso-m.png')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }
-        }else if(fsex[1].checked){
-            genero='f'
-            if(idade >= 0 && idade < 10){
-                img.setAttribute('src', 'img/bebe-f.jpg')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }else if(idade < 21){
-                img.setAttribute('src', 'img/jovem-f.png')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }else if(idade < 75){
-                img.setAttribute('src', 'img/adulta-f.png')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }else{
-                img.setAttribute('src', 'img/idosa-f.png')
-                img.setAttribute('width', 250)
-                img.setAttribute('height', 250)
-                img.style.borderRadius = '50%';
-            }
-        }
-        res.style.textAlign='center'
-        res.innerHTML=`Idade calculada: ${idade} e Genero: ${genero}`
-        res.appendChild(img)
+        res.innerHTML='<p>aaa</p>'
     }
 }
