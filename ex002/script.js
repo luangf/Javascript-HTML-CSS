5041c7185dd13401423f549b7b81fc39ef139c3f
Luan <luangf222@gmail.com>
2023-06-15 03:03:48 -0300

    att

---
+++ ex002/script.js
@@ -0,0 +1,16 @@
+var count=1
+while(count < 5){
+    console.log(2)
+    count++
+}
+console.log('----------')
+count=1
+do{
+    console.log(2)
+    count++
+}while(count < 5)
+console.log('----------')
+count=1
+for(var i=1;i<5;i++){
+    console.log(2)
+}
