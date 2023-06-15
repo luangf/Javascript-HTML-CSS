b26e854a6c8b9ec11c68260e992c747e523939f5
Luan <luangf222@gmail.com>
2023-06-15 13:24:40 -0300

    att

--- projeto3/script.js
+++ projeto3/script.js
@@ -1,16 +1,27 @@
 function contar(){
-    var inicio=document.querySelector('input#inicio')
-    var fim=document.querySelector('input#fim')
-    var passo=document.querySelector('input#passo')
-    var all=`${inicio.value} ${fim.value} ${passo.value}`
-    // alert(all)
-    if(inicio.value == ''){
+    var inicio=document.querySelector('#inicio').value
+    var fim=document.querySelector('#fim').value
+    var passo=document.querySelector('#passo').value
+    var res=document.querySelector('#res')
+    if(inicio == '' || fim == '' || passo == ''){
         res.innerHTML='<p>Impossível contar</p>'
-    }else if(fim.value == ''){
-        res.innerHTML='<p>Impossível contar</p>'
-    }else if(passo.value == ''){
-    res.innerHTML='<p>Impossível contar</p>'
     }else{
-        res.innerHTML='<p>aaa</p>'
+        res.innerHTML='<p>Contando<br></p>'
+        if(passo<=0){
+            window.alert('Passo inválido, setei p=1')
+            passo=1
+        }
+        if(inicio < fim){
+            // contagem crescente
+            for(let i=Number(inicio); i<=Number(fim); i+=Number(passo)){
+                res.innerHTML+=`${i} \u{1F449}`
+            }
+        }else{
+            // contagem descrescente
+            for(let i=Number(inicio); i>=Number(fim); i-=Number(passo)){
+                res.innerHTML+=`${i} \u{1F449}`
+            }
+        }
+        res.innerHTML+='\u{1F44D}'
     }
 }
